cd06da13317d2883f4a1c5786f11d5d814c69017
Kaua1102bit <kauarsfavika@gmail.com>
2025-10-03 11:28:15 -0300

    03.10

---
+++ Hardware/sa_geolocator_maps/lib/models/location_points.dart
@@ -0,0 +1,13 @@
+class LocationPoints {
+  //atributos
+  final double latitude;
+  final double longitude;
+  final String timeStamp;
+
+  //construtor
+  LocationPoints({
+    required this.latitude, required this.longitude, required this.timeStamp,
+  });
+
+  
+}
