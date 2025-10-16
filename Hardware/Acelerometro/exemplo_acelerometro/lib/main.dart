3d5a143b23dd16815605f77f7aa18fda3f28bb44
Kaua1102bit <kauarsfavika@gmail.com>
2025-10-16 13:11:26 -0300

    16.10.25

--- Hardware/Acelerometro/exemplo_acelerometro/lib/main.dart
+++ Hardware/Acelerometro/exemplo_acelerometro/lib/main.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
-void main(){
-  runApp(MaterialApp(home: AcelerometroScreen(),));
+void main() {
+  runApp(MaterialApp(home: AcelerometroScreen()));
 }
 
 class AcelerometroScreen extends StatefulWidget {
@@ -16,65 +15,38 @@ class AcelerometroScreen extends StatefulWidget {
 
 class _AcelerometroScreenState extends State<AcelerometroScreen> {
   List<double>? _acelerometroValues;
-
   StreamSubscription<AccelerometerEvent>? _acelerometroSubscription;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    _acelerometroSubscription = accelerometerEvents.listen((AccelerometerEvent event) {
-      setState(() {
-        _acelerometroValues = <double>[event.x, event.y, event.z];
-      });
-    });
-    
-  }
-  
-  @overrideaimport 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:sensors_plus/sensors_plus.dart';
-
-void main(){
-  runApp(MaterialApp(home: AcelerometroScreen(),));
-}
-
-class AcelerometroScreen extends StatefulWidget {
-  const AcelerometroScreen({super.key});
-
-  @override
-  State<AcelerometroScreen> createState() => _AcelerometroScreenState();
-}
-
-class _AcelerometroScreenState extends State<AcelerometroScreen> {
-  List<double>? _acelerometroValues;
-
-  StreamSubscription<AccelerometerEvent>? _acelerometroSubscription;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _acelerometroSubscription = accelerometerEventStream().listen((AccelerometerEvent event){
+    _acelerometroSubscription = accelerometerEvents.listen((
+      AccelerometerEvent event,
+    ) {
       setState(() {
         _acelerometroValues = <double>[event.x, event.y, event.z];
       });
     });
   }
-  
+
+  @override
+  void dispose() {
+    _acelerometroSubscription?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Acelerometro"),),
+      appBar: AppBar(title: Text("Acelerômetro")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text("Valores do Acelerômetro"),
             Text("Eixo X: ${_acelerometroValues?[0]}"),
-            Text("Eixo y: ${_acelerometroValues?[1]}"),
-            Text("Eixo z: ${_acelerometroValues?[2]}"),
+            Text("Eixo Y: ${_acelerometroValues?[1]}"),
+            Text("Eixo Z: ${_acelerometroValues?[2]}"),
           ],
         ),
       ),
